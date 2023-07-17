5f44da2414bb78d20a44d4834b2eedd5a1e1136f
Danilo Gila <danilo.santana@g.globo>
2023-07-17 00:12:32 -0300

    first commit

---
+++ src/components/Text/Text.tsx
@@ -0,0 +1,99 @@
+import {createText} from '@shopify/restyle';
+import React from 'react';
+import {TextStyle} from 'react-native';
+import {Theme} from '../../theme/theme';
+
+const SRText = createText<Theme>();
+
+type SRTextProps = React.ComponentProps<typeof SRText>;
+
+interface TextProps extends SRTextProps {
+  preset?: TextVariants;
+  bold?: boolean;
+  italic?: boolean;
+  semiBold?: boolean;
+}
+
+export function Text({
+  children,
+  style,
+  preset = 'paragraphMedium',
+  bold,
+  italic,
+  semiBold,
+  ...sRTextProps
+}: TextProps) {
+  const fontFamily = getFontFamily(preset, bold, italic, semiBold);
+  return (
+    <SRText
+      color="backgroundContrast"
+      style={[$fontSizes[preset], {fontFamily}, style]}
+      {...sRTextProps}>
+      {children}
+    </SRText>
+  );
+}
+
+function getFontFamily(
+  preset: TextVariants,
+  bold?: boolean,
+  italic?: boolean,
+  semiBold?: boolean,
+) {
+  if (['headingLarge', 'headingMedium', 'headingSmall'].includes(preset)) {
+    return italic ? $fontFamily.boldItalic : $fontFamily.bold;
+  }
+  if (bold && italic) {
+    return $fontFamily.boldItalic;
+  }
+  if (bold) {
+    return $fontFamily.bold;
+  }
+  if (italic) {
+    return $fontFamily.italic;
+  }
+  if (semiBold && italic) {
+    return $fontFamily.mediumItalic;
+  }
+  if (semiBold) {
+    return $fontFamily.medium;
+  }
+
+  return $fontFamily.regular;
+}
+
+type TextVariants =
+  | 'headingLarge'
+  | 'headingMedium'
+  | 'headingSmall'
+  | 'paragraphLarge'
+  | 'paragraphMedium'
+  | 'paragraphSmall'
+  | 'paragraphCaption'
+  | 'paragraphCaptionSmall';
+
+const $fontSizes: Record<TextVariants, TextStyle> = {
+  headingLarge: {fontSize: 32, lineHeight: 38.4},
+  headingMedium: {fontSize: 22, lineHeight: 26.4},
+  headingSmall: {fontSize: 18, lineHeight: 23.4},
+
+  paragraphLarge: {fontSize: 18, lineHeight: 25.2},
+  paragraphMedium: {fontSize: 16, lineHeight: 22.4},
+  paragraphSmall: {fontSize: 14, lineHeight: 19.6},
+
+  paragraphCaption: {fontSize: 12, lineHeight: 16.8},
+  paragraphCaptionSmall: {fontSize: 10, lineHeight: 14},
+};
+
+const $fontFamily = {
+  black: 'Satoshi-Black',
+  blackItalic: 'Satoshi-BlackItalic',
+  bold: 'Satoshi-Bold',
+  boldItalic: 'Satoshi-BoldItalic',
+  italic: 'Satoshi-Italic',
+  light: 'Satoshi-Light',
+  lightItalic: 'Satoshi-LightItalic',
+  medium: 'Satoshi-Medium',
+  mediumItalic: 'Satoshi-MediumItalic',
+  regular: 'Satoshi-Regular',
+};
